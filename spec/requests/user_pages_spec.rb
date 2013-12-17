4ca9d782f44fed249acaa638e397c487a4daa2f8
jccook <jccook@madisoncollege.edu>
2013-12-16 23:29:03 -0600

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -85,7 +85,6 @@ describe "User pages" do
         it { should have_title('Sign up') }
         it { should have_content('error') }
       end
-
     end
 
     describe "with valid information" do
@@ -107,9 +106,7 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
-
     end
-
   end
 
   describe "edit" do
@@ -144,5 +141,20 @@ describe "User pages" do
     end
 
   end
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
 
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
+  end
 end
